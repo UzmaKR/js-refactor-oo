467aaf14122f3bd10a8b1d4ca32031abc0f87a3f
Apprentice <apprentice@dbc03.local>
2013-06-15 20:49:43 -0700

    adding functions to Cup and Die objects

--- application.js
+++ application.js
@@ -1,42 +1,43 @@
 function Die(numSides, value) {
   this.numSides = numSides;
   this.value = value;
-};
+}
 
 function Cup() {
   this.bunch = [];
 }
 
-function drawDie() {
+Die.prototype.drawDie = function() {
   $('.dice').append('<div class="die">0</div>');
 };
 
-function drawAllDie(cup) {
+
+Cup.prototype.drawAllDie = function(cup) {
   $('.die').each(function(k, die) {
      $(die).text(cup.bunch[k].value); 
    });
 };
 
-function addDieToCup(numSides, cuppa) {
+Cup.prototype.addDieToCup = function(numSides, cuppa) {
     var die = new Die(numSides, 0);
     cuppa.bunch.push(die);
-    drawDie();
+    die.drawDie();
 };
 
-function rollAllDie(cup) {
+Cup.prototype.rollAllDie = function(cup) {
   cup.bunch.forEach(function(die,k) {
     die.value = Math.floor((Math.random()*die.numSides)+1);
   });
-  drawAllDie(cup);
+  cup.drawAllDie(cup);
 };
 
 function bindEvents(numSides, cuppa) {
     $('#roller button.add').on('click', function() {
-        addDieToCup(numSides, cuppa);
+      cuppa.addDieToCup(numSides, cuppa);
     });
 
     $('#roller button.roll').on('click', function() {
-      rollAllDie(cuppa);
+      cuppa.rollAllDie(cuppa);
     });
 };
 
