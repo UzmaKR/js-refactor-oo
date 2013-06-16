7aa1752aa1b93947ad28490e4fea1b8b8d35c190
Apprentice <apprentice@dbc03.local>
2013-06-15 21:08:37 -0700

    removed parameters and used this to refer to cup object

--- application.js
+++ application.js
@@ -3,41 +3,41 @@ function Die(numSides, value) {
   this.value = value;
 }
 
-function Cup() {
-  this.bunch = [];
-}
-
 Die.prototype.drawDie = function() {
   $('.dice').append('<div class="die">0</div>');
 };
 
+function Cup() {
+  this.bunch = [];
+}
 
-Cup.prototype.drawAllDie = function(cup) {
+Cup.prototype.drawAllDie = function() {
+  cup = this;
   $('.die').each(function(k, die) {
      $(die).text(cup.bunch[k].value); 
    });
 };
 
-Cup.prototype.addDieToCup = function(numSides, cuppa) {
+Cup.prototype.addDieToCup = function(numSides) {
     var die = new Die(numSides, 0);
-    cuppa.bunch.push(die);
+    this.bunch.push(die);
     die.drawDie();
 };
 
-Cup.prototype.rollAllDie = function(cup) {
-  cup.bunch.forEach(function(die,k) {
+Cup.prototype.rollAllDie = function() {
+  this.bunch.forEach(function(die,k) {
     die.value = Math.floor((Math.random()*die.numSides)+1);
   });
-  cup.drawAllDie(cup);
+  this.drawAllDie();
 };
 
 function bindEvents(numSides, cuppa) {
     $('#roller button.add').on('click', function() {
-      cuppa.addDieToCup(numSides, cuppa);
+      cuppa.addDieToCup(numSides);
     });
 
     $('#roller button.roll').on('click', function() {
-      cuppa.rollAllDie(cuppa);
+      cuppa.rollAllDie();
     });
 };
 
